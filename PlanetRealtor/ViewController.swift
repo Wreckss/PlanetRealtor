b559933932dc471b51c4cea29762053b772a2f22
tacoRiot <46939881+tacoRiot@users.noreply.github.com>
2019-07-28 16:03:17 -0600

    uploading

--- PlanetRealtor/ViewController.swift
+++ PlanetRealtor/ViewController.swift
@@ -20,15 +20,25 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
         
+        
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let moon = SCNSphere(radius: 0.2)
+        let material = SCNMaterial()
         
-        // Set the scene to the view
-        sceneView.scene = scene
-    }
+        material.diffuse.contents = UIImage(named: "iu.jpeg")
+        moon.materials = [material]
+        
+        let node = SCNNode()
+        node.position = SCNVector3Make(0, 0.1, -0.5)
+        
+        node.geometry = moon
+        
+        sceneView.scene.rootNode.addChildNode(node)
+        sceneView.automaticallyUpdatesLighting = true 
+        }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
